a07a9ee578331bc8fdcbfe575bbb703a2846fcee
Keshan Chetty <keshan@localhost.localdomain>
2017-01-05 23:23:45 +0200

    hahaha

--- loop.cpp
+++ loop.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -8,7 +9,7 @@ int main()
 {
 	int array[19][19];
 	int y, x;
-	vector < vector <int> > co_ordinates[400, 0];
+	vector <vector <int> > co_ords;
 
 	for (y = 0; y < 19; y++)
 	{
@@ -40,24 +41,73 @@ int main()
 			cout << array[y][x] << " ";
 		}   
 		cout << endl;
-	}  
-		
+	} 
+	
+	vector<int> temp;
+	temp.push_back(0);
+	temp.push_back(0);
 	for (y = 0; y < 19; y++)
 	{
 		for (x = 0; x < 19; x++)
-		{
+		{ 
 			if (array[y][x + 1] == -1 && array[y][x] == 1 || array[y][x] == 2)
 			{
-				
+					temp[0] = x + 1;
+					temp[1] = y;
+					co_ords.push_back(temp);
 			}
 			if (array[y + 1][x] == -1 && array[y][x] == 1 || array[y][x] == 2)
+			{
+					temp[0] = x;                                                 
+			    temp[1] = y + 1;  
+					co_ords.push_back(temp);
+			}
 			if (array[y - 1][x] == -1 && array[y][x] == 1 || array[y][x] == 2)
+			{
+					temp[0] = x;                                                 
+			    temp[1] = y - 1;     
+					co_ords.push_back(temp);
+			}
 			if (array[y][x - 1] == -1 && array[y][x] == 1 || array[y][x] == 2)
+			{
+					temp[0] = x - 1;                                                 
+			    temp[1] = y;     
+					co_ords.push_back(temp);
+			}
 			if (array[y + 1][x + 1] == -1 && array[y][x] == 1 || array[y][x] == 2)
+			{
+					temp[0] = x + 1;                                                 
+			    temp[1] = y + 1;     
+					co_ords.push_back(temp);
+			}
 			if (array[y - 1][x + 1] == -1 && array[y][x] == 1 || array[y][x] == 2)
+			{
+					temp[0] = x + 1;                                                 
+			    temp[1] = y - 1;     
+					co_ords.push_back(temp);
+			}
 			if (array[y - 1][x - 1] == -1 && array[y][x] == 1 || array[y][x] == 2)
+			{
+					temp[0] = x - 1;                                                 
+			    temp[1] = y - 1;     
+					co_ords.push_back(temp);
+			}
 			if (array[y + 1][x - 1] == -1 && array[y][x] == 1 || array[y][x] == 2)
+			{
+					temp[0] = x - 1;                                                 
+			   	temp[1] = y + 1;     
+					co_ords.push_back(temp);
+			}
 		}
+
+		cout << endl;
+		for (int i = 0; i < co_ords.size(); i++)
+		{
+				cout << " " << co_ords[i][0] << endl;	
+				cout << " " << co_ords[i][1] << endl;
+				cout << endl << endl;
+		}
+
 	}
 
 	return (0);
